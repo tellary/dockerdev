163ee200755e6e924da81dd74d21827f7ea8748f
Ilya Silvestrov <tellary@gmail.com>
2017-12-06 15:24:51 -0800

    Create `.gradle` volume to share Gradle dependencies

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ docker run -it \
        -v /Users/ilya/safeplace:/home/ilya/safeplace \
        -v /Users/ilya/shared:/home/ilya/shared \
        -v /Users/ilya/Downloads:/home/ilya/Downloads \
+       -v /Users/ilya/.gradle:/home/ilya/.gradle \
        -u ilya \
        -e DISPLAY=123.123.123.123:0 \
        --rm \
