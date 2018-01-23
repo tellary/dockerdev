b005e33e127155e3eb10db48b07c55a92ce0cbdd
Ilya Silvestrov <tellary@gmail.com>
2018-01-22 19:14:21 -0800

    Add ~/.ssh volume to share the key

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker run -it \
        -v /Users/ilya/shared:/home/ilya/shared \
        -v /Users/ilya/Downloads:/home/ilya/Downloads \
        -v /Users/ilya/.gradle:/home/ilya/.gradle \
+       -v /Users/ilya/.ssh:/home/ilya/.ssh \
        -u ilya \
        -e DISPLAY=123.123.123.123:0 \
        --rm \
