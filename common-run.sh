dbcd6d35b4caddf5a0e1386ac3874f9cca42e0d5
Ilya Silvestrov <tellary@gmail.com>
2019-03-09 05:02:00 -0800

    Removed network alias.
    Use `host.docker.internal` insted.

--- common-run.sh
+++ common-run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 BASE_DIR=$HOME
-xhost + 123.123.123.123
 DOCKER_OPTS="-it \
        -v $BASE_DIR/safeplace:/home/ilya/safeplace \
        -v $BASE_DIR/shared:/home/ilya/shared \
        -v $BASE_DIR/Downloads:/home/ilya/Downloads \
        -v $BASE_DIR/Desktop:/home/ilya/Desktop \
        -v $BASE_DIR/.ssh:/home/ilya/.ssh \
-       -e DISPLAY=123.123.123.123:0"
+       -e DISPLAY=host.docker.internal:0"
--- set_alias.sh
+++
@@ -1,1 +0,0 @@
-sudo ifconfig lo0 alias 123.123.123.123/24
