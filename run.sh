25b67fd8a7e33fff71a0acc54ae9c2e8af384cdc
Ilya Silvestrov <tellary@gmail.com>
2019-07-03 17:58:48 -0700

    Add `~/.aws` volume

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 docker run -it $DOCKER_OPTS \
        -v $BASE_DIR/work:/home/ilya/work \
        -v dot-gradle:/home/ilya/.gradle \
+       -v $BASE_DIR/.aws:/home/ilya/.aws \
        -u ilya \
        $@ \
        $WORK_IMAGE
